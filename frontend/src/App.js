dc52ee2f2723d7eec03a50ce2a273a9777f56d79
gkshuvo <gkshuvo24@gmail.com>
2023-02-21 09:24:11 +0600

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,37 +18,35 @@ import UserEdit from "./screens/UserEdit";
 import ProductList from "./screens/ProductList";
 import ProductEdit from "./screens/ProductEdit";
 import OrderList from "./screens/OrderList";
-import CheckoutScreen from "./screens/CheckoutScreen";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Route path="/order/:id" component={Order} />
-      <Route path="/shipping" component={Shipping} />
-      <Route path="/payment" component={PaymentMethod} />
-      <Route path="/placeorder" component={PlaceOrder} />
-      {/* <Route path="/makepayment" component={CheckoutScreen} /> */}
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/product/:id" component={ProductDetails} />
-      <Route path="/cart/:id?" component={Cart} />
-      <Route path="/admin/userlist" component={UserList} />
-      <Route path="/admin/user/:id/edit" component={UserEdit} />
-      <Route path="/admin/productlist" component={ProductList} exact />
+      <Route path='/order/:id' component={Order} />
+      <Route path='/shipping' component={Shipping} />
+      <Route path='/payment' component={PaymentMethod} />
+      <Route path='/placeorder' component={PlaceOrder} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+      <Route path='/profile' component={Profile} />
+      <Route path='/product/:id' component={ProductDetails} />
+      <Route path='/cart/:id?' component={Cart} />
+      <Route path='/admin/userlist' component={UserList} />
+      <Route path='/admin/user/:id/edit' component={UserEdit} />
+      <Route path='/admin/productlist' component={ProductList} exact />
       <Route
-        path="/admin/productlist/:pageNumber"
+        path='/admin/productlist/:pageNumber'
         component={ProductList}
         exact
       />
-      <Route path="/admin/product/:id/edit" component={ProductEdit} />
-      <Route path="/admin/orderlist" component={OrderList} />
-      <Route path="/shop" component={Shop} exact />
-      <Route path="/search/:keyword" component={Shop} exact />
-      <Route path="/page/:pageNumber" component={Shop} exact />
-      <Route path="/search/:keyword/page/:pageNumber" component={Shop} exact />
-      <Route path="/" component={Home} exact />
+      <Route path='/admin/product/:id/edit' component={ProductEdit} />
+      <Route path='/admin/orderlist' component={OrderList} />
+      <Route path='/shop' component={Shop} exact />
+      <Route path='/search/:keyword' component={Shop} exact />
+      <Route path='/page/:pageNumber' component={Shop} exact />
+      <Route path='/search/:keyword/page/:pageNumber' component={Shop} exact />
+      <Route path='/' component={Home} exact />
 
       <Footer />
     </Router>
